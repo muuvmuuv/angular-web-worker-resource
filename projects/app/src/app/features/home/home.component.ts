1a2c07344e974e79eff941b8b5396ab9c14add96
Marvin Heilemann <11534760+muuvmuuv@users.noreply.github.com>
2026-02-19 23:21:20 +0100

    fix(app): debounce input to avoid queuing expensive computations

--- projects/app/src/app/features/home/home.component.ts
+++ projects/app/src/app/features/home/home.component.ts
@@ -1,6 +1,8 @@
 import { ChangeDetectionStrategy, Component, signal } from "@angular/core"
+import { toObservable, toSignal } from "@angular/core/rxjs-interop"
 import { FormsModule } from "@angular/forms"
 import { webWorkerResource } from "angular-web-worker-resource"
+import { debounceTime } from "rxjs"
 
 @Component({
 	selector: "app-feature-home",
@@ -11,9 +13,15 @@ import { webWorkerResource } from "angular-web-worker-resource"
 })
 export class HomeFeature {
 	readonly input = signal(40)
+	readonly debouncedInput = toSignal(
+		toObservable(this.input).pipe(debounceTime(500)),
+		{
+			initialValue: this.input(),
+		},
+	)
 
 	readonly fibonacci = webWorkerResource<number, number>({
-		params: () => this.input(),
+		params: () => this.debouncedInput(),
 		worker: () => new Worker(new URL("./fibonacci.worker", import.meta.url)),
 		defaultValue: 0,
 	})
